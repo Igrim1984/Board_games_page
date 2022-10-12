aad71a99e751ba42c4c49b7ca75f3d545578d5b9
Łukasz Czołczyński <lukasz.czolczynski84@gmail.com>
2022-10-12 18:05:03 +0200

    I added an image gallery with javaScript support

--- app.js
+++ app.js
@@ -1,7 +1,96 @@
-const tl = gsap.timeline({defaults:{ease:"power1.out"}});
+// <----------------------- Intro -------------------------->
 
-tl.to(".text", {y:"0%", duration:1, stagger: 0.25});
-tl.to(".slider", {y: "-100%", duration: 1.5, delay: 0.5});
-tl.to('.intro', {y: "-100%", duration: 1.5}, "-=1");
-tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1 })
-tl.fromTo('big-text', {opacity: 0}, {opacity: 1, duration: 1 }, "-=1")
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1.5 }, "-=1");
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo("big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+
+// <----------------------- Slider -------------------------->
+
+const THUMBNAIL = document.querySelectorAll(".thumbnail img");
+const POPUP = document.querySelector(".popup");
+const POPUP_CLOSE = document.querySelector(".popup__close");
+const POPUP_IMG = document.querySelector(".popup__img");
+const ARROW_LEFT = document.querySelector(".popup__arrow--left");
+const ARROW_RIGHT = document.querySelector(".popup__arrow--right");
+
+let currentImgIndex;
+
+const showNextImg = () => {
+  if (currentImgIndex === THUMBNAIL.length - 1) {
+    currentImgIndex = 0;
+  } else {
+    currentImgIndex++;
+  }
+  POPUP_IMG.src = THUMBNAIL[currentImgIndex].src;
+};
+
+const showPreviousImg = () => {
+  if (currentImgIndex === 0) {
+    currentImgIndex = THUMBNAIL.length - 1;
+  } else {
+    currentImgIndex--;
+  }
+  POPUP_IMG.src = THUMBNAIL[currentImgIndex].src;
+};
+
+const closePopup = () => {
+  POPUP.classList.add("fadeOut");
+  setTimeout(() => {
+    POPUP.classList.add("hidden");
+    POPUP.classList.remove("fadeOut");
+    THUMBNAIL.forEach((element) => {
+      element.setAttribute("tabindex", 1);
+    });
+  }, 500);
+};
+
+THUMBNAIL.forEach((thumbnail, index) => {
+  const showPopup = (e) => {
+    POPUP.classList.remove("hidden");
+    POPUP_IMG.src = e.target.src;
+    currentImgIndex = index;
+    THUMBNAIL.forEach((element) => {
+      element.setAttribute("tabindex", -1);
+    });
+  };
+
+  thumbnail.addEventListener("click", showPopup);
+
+  thumbnail.addEventListener("keydown", (e) => {
+    if (e.code === "Enter" || e.keycode === 13) {
+      showPopup(e);
+    }
+  });
+});
+
+POPUP_CLOSE.addEventListener("click", closePopup);
+
+ARROW_RIGHT.addEventListener("click", showNextImg);
+
+ARROW_LEFT.addEventListener("click", showPreviousImg);
+
+document.addEventListener("keydown", (e) => {
+  if (!POPUP.classList.contains("hidden")) {
+    if (e.code === "ArrowRight" || e.keycode === 39) {
+      showNextImg();
+    }
+
+    if (e.code === "ArrowLeft" || e.keycode === 37) {
+      showPreviousImg();
+    }
+
+    if (e.code === "Escape" || e.keycode === 27) {
+      closePopup();
+    }
+  }
+});
+
+POPUP.addEventListener("click", (e) => {
+  if (e.target === POPUP) {
+    closePopup();
+  }
+});
